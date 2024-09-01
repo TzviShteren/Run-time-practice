65b4987348e9e2b6f8e61d416cff07ea2bd8e785
tzevi stern <z02072000@gmail.com>
2024-09-01 10:24:13 +0300

    Bubble sort

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static public void BabelSors(int[] ints)
+        {
+            bool isDon = false;
+            for (int i = 0; i < ints.Length - 1; i++)
+            {
+                if (ints[i] > ints[i + 1])
+                {
+                    int tamp = ints[i];
+                    ints[i] = ints[i + 1];
+                    ints[i + 1] = tamp;
+                    isDon = true;
+                }
+                if(!isDon)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
